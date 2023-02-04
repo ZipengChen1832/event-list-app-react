03970521687053f17bc96a86c54d6969e5d18f96
zipengchen1832 <zipengchen1832@gmail.com>
2023-02-04 17:53:18 -0500

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: "inline-source-map",
-    mode: "development",
+    mode: "production",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
