cfa2750f81a2b06eeede3731fab05308e8df8d0a
zipengchen1832 <zipengchen1832@gmail.com>
2023-02-04 17:46:51 -0500

    added build folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: "inline-source-map",
     mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   module: {
     rules: [
